9eea36df1f5b832f3c3071ff41113e3dc61850ee
MSouch <msouch3@gmail.com>
2024-03-12 09:09:01 -0600

    Add project files.

---
+++ SerializationExample/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationExample
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Student()
+        { }
+        public override string ToString()
+        {
+            return $"student detials are: "+
+                $"Name: {Name}, " +
+                $"Age: {Age}, " +
+                $"Username: {Username}, " +
+                $"Password: {Password}";
+        }
+    }
+}
---
+++ SerializationExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializationExample;
+
+class Program
+{
+#pragma warning disable SYSLIB0011 // Variable is declared but never used
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");  
+        string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        Console.WriteLine(path);
+        Student s = new Student()
+        {
+            Name = "John",
+            Age = 25,
+            Username = "john123",
+            Password = "123456",
+        };
+
+        string pathBin = path + "student.bin";
+
+    }   
+    public static void SerializeStudent(string path, Student s)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using(FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+        {
+            binaryFormatter.Serialize(fs, s);
+        }
+        Console.WriteLine("Student object serialized successfully");
+    }
+    private static void DeserializeStudent(string path)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using(FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            Student sl = (Student)binaryFormatter.Deserialize(fs);
+            Console.WriteLine(sl);
+        }
+    }
+}
